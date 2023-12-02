e006c05dacd9518ccd097d89d381cf659c4673a9
Yaroslav <jlamttobar.trh@gmail.com>
2023-12-03 01:25:15 +0300

    img fix

--- home.php
+++ home.php
@@ -155,44 +155,45 @@ Template Name: home
             <!--// end row -->
 
             <div class="row">
-                <!-- Latest Products -->
-                <div class="col-sm-4 sm-margin-b-50">
-                    <div class="margin-b-20">
-                        <div class="wow zoomIn" data-wow-duration=".3" data-wow-delay=".1s">
-                            <img class="img-responsive" src="<?php bloginfo('template_url') ?>/assets/img/970x647/01.jpg" alt="Latest Products Image">
-                        </div>
-                    </div>
-                    <h4><a href="#">Triangle Roof</a> <span class="text-uppercase margin-l-20">Management</span></h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed tempor incdidunt ut laboret dolor magna ut consequat siad esqudiat dolor</p>
-                    <a class="link" href="#">Read More</a>
-                </div>
-                <!-- End Latest Products -->
+                
+            <?php
+                global $post;
 
-                <!-- Latest Products -->
-                <div class="col-sm-4 sm-margin-b-50">
-                    <div class="margin-b-20">
-                        <div class="wow zoomIn" data-wow-duration=".3" data-wow-delay=".1s">
-                            <img class="img-responsive" src="<?php bloginfo('template_url') ?>/assets/img/970x647/02.jpg" alt="Latest Products Image">
-                        </div>
-                    </div>
-                    <h4><a href="#">Curved Corners</a> <span class="text-uppercase margin-l-20">Developmeny</span></h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed tempor incdidunt ut laboret dolor magna ut consequat siad esqudiat dolor</p>
-                    <a class="link" href="#">Read More</a>
-                </div>
-                <!-- End Latest Products -->
+                $myposts = get_posts([ 
+	                    'numberposts' => -1,
+	                    
+                ]);
 
-                <!-- Latest Products -->
-                <div class="col-sm-4 sm-margin-b-50">
-                    <div class="margin-b-20">
-                        <div class="wow zoomIn" data-wow-duration=".3" data-wow-delay=".1s">
-                            <img class="img-responsive" src="<?php bloginfo('template_url') ?>/assets/img/970x647/03.jpg" alt="Latest Products Image">
+                if( $myposts ){
+                	foreach( $myposts as $post ){
+                		setup_postdata( $post );
+                		?>
+                		<!-- Latest Products -->
+                        <div class="col-sm-4 sm-margin-b-50">
+                            <div class="margin-b-20">
+                                <div class="wow zoomIn" data-wow-duration=".3" data-wow-delay=".1s">
+                                    <!-- <img class="img-responsive" src="<?php bloginfo('template_url') ?>/assets/img/970x647/01.jpg" alt="Latest Products Image"> -->
+                                    <?php the_post_thumbnail(
+                                        array(970,647),
+                                        array(
+                                            'class' => 'img-responsive'
+                                        )
+                                    ); ?>
+                                </div>
+                            </div>
+                            <h4><a href="#"><?php the_title();?></a> <span class="text-uppercase margin-l-20">Management</span></h4>
+                            <p><?php the_content(); ?></p>
+                            <a class="link" href="#">Read More</a>
                         </div>
-                    </div>
-                    <h4><a href="#">Bird On Green</a> <span class="text-uppercase margin-l-20">Design</span></h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed tempor incdidunt ut laboret dolor magna ut consequat siad esqudiat dolor</p>
-                    <a class="link" href="#">Read More</a>
-                </div>
-                <!-- End Latest Products -->
+                        <!-- End Latest Products -->
+                		<?php 
+                	}
+                }
+
+                wp_reset_postdata(); // Сбрасываем $post
+            ?>
+                
+
             </div>
             <!--// end row -->
         </div>
